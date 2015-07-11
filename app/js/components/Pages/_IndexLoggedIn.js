ffb0b2686cc5ccadee9f56d2105df482876b1717
Robin Malfait <malfait.robin@gmail.com>
2015-07-11 15:25:31 +0200

    Adding materialize vendor

--- app/js/components/Pages/_IndexLoggedIn.js
+++ app/js/components/Pages/_IndexLoggedIn.js
@@ -3,8 +3,72 @@ import React from 'react'
 var _IndexLoggedIn = React.createClass({
     render() {
         return (
-            <div>
-                <h1>Teeheee</h1>
+            <div className="container">
+                <div className="section">
+                    <div className="right-align">
+                        <a className="btn-floating btn-medium waves-effect waves-light red"><i className="material-icons">add</i></a>
+                    </div>
+
+                    <div className="shout hoverable">
+                        <div className="shout-header">
+                            <a href="#">
+                                <img src="https://avatarize.me/a/mike.brants@gmail.com" alt="" className="avatar"/>
+                            </a>
+                            <span className="shout-title"> shout Title </span>
+                        </div>
+                        <div className="shout-content">
+                            <p className="black-text">I am a very simple shout. I am good at containing small bits of information. I am convenient because I require little markup to use effectively. I am a very simple shout. I am good at containing small bits of information. I am convenient because I require little markup to use effectively. I am a very simple shout. I am good at containing small bits of information. I am convenient because I require little markup to use effectively. I am a very simple shout. I am good at containing small bits of information. I am convenient because I require little markup to use effectively. I am a very simple shout. I am good at containing small bits of information. I am convenient because I require little markup to use effectively. I am a very simple shout. I am good at containing small bits of information. I am convenient because I require little markup to use effectively. I am a very simple shout. I am good at containing small bits of information. I am convenient because I require little markup to use effectively. I am a very simple shout. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                        </div>
+                        <div className="shout-footer right-align">
+                            <a href="#"><i className="material-icons">grade</i></a>
+                        </div>
+                        <div className="shout-progress">
+                            <div className="progress">
+                                <div className="determinate" style={{width: '70%'}}></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="shout hoverable">
+                        <div className="shout-header">
+                            <a href="#">
+                                <img src="https://avatarize.me/a/mike.brants@gmail.com" alt="" className="avatar" />
+                            </a>
+                            <span className="shout-title"> shout Title </span>
+                        </div>
+                        <div className="shout-content">
+                            <p className="black-text">I am a very simple shout. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                        </div>
+                        <div className="shout-footer right-align">
+                            <a href="#"><i className="material-icons">grade</i></a>
+                        </div>
+                        <div className="shout-progress">
+                            <div className="progress">
+                                <div className="determinate" style={{width: '70%'}}></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="shout hoverable">
+                        <div className="shout-header">
+                            <a href="#">
+                                <img src="https://avatarize.me/a/mike.brants@gmail.com" alt="" className="avatar" />
+                            </a>
+                            <span className="shout-title"> shout Title </span>
+                        </div>
+                        <div className="shout-content">
+                            <p className="black-text">I am a very simple shout. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                        </div>
+                        <div className="shout-footer right-align">
+                            <a href="#"><i className="material-icons">grade</i></a>
+                        </div>
+                        <div className="shout-progress">
+                            <div className="progress">
+                                <div className="determinate" style={{width: '70%'}}></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
