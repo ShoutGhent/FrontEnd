c8750d4165d149c997e5aeae8482b6d21d01c473
Robin Malfait <malfait.robin@gmail.com>
2015-08-02 13:16:05 +0200

    removing tabs for now

--- src/js/components/pages/profile/Settings.js
+++ src/js/components/pages/profile/Settings.js
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import Avatar from '../../users/Avatar'
 import EditName from './EditName'
 import InfoPanel from '../../partials/InfoPanel'
@@ -6,7 +7,7 @@ import LoginStore from '../../../auth/LoginStore'
 import MaterialInput from '../../partials/MaterialInput'
 import { Card, CardContent, CardTitle } from '../../card/Card'
 import { Grid, Cell } from '../../grid/Grid'
-import { Tab, TabPanel } from '../../tab/Tab'
+
 
 let Settings = React.createClass({
     getInitialState() {
@@ -25,37 +26,31 @@ let Settings = React.createClass({
         let { user } = this.state
 
         return (
-            <div>
-                <div className="settings container">
-                    <Tab>
-                        <TabPanel title="Persoonlijk">
-                            <Grid>
-                                <Cell width={6/12}>
-                                    <EditName user={user}/>
-                                </Cell>
-                                <Cell width={6/12}>
-                                    <Card>
-                                        <CardContent>
-                                            <CardTitle>Foto Wijzigen</CardTitle>
+            <div className="container">
+                <Grid>
+                    <Cell width={6/12}>
+                        <EditName user={user}/>
+                    </Cell>
+                    <Cell width={6/12}>
+                        <Card>
+                            <CardContent>
+                                <CardTitle>Foto Wijzigen</CardTitle>
 
-                                            <Grid>
-                                                <Cell center>
-                                                    <Avatar email={user.email} size={100} round/>
-                                                </Cell>
-                                                <Cell>
-                                                    <InfoPanel>
-                                                        Je kan je afbeelding wijzigen op: <a href={`https://avatarize.me/?email=${user.email}`} target="_blank">avatarize.me</a>,
-                                                        heb je nog geen account, maak dan een met het volgende email adres: <strong>{user.email}</strong>
-                                                    </InfoPanel>
-                                                </Cell>
-                                            </Grid>
-                                        </CardContent>
-                                    </Card>
-                                </Cell>
-                            </Grid>
-                        </TabPanel>
-                    </Tab>
-                </div>
+                                <Grid>
+                                    <Cell center>
+                                        <Avatar email={user.email} size={100} round/>
+                                    </Cell>
+                                    <Cell>
+                                        <InfoPanel>
+                                            Je kan je afbeelding wijzigen op: <a href={`https://avatarize.me/?email=${user.email}`} target="_blank">avatarize.me</a>,
+                                            heb je nog geen account, maak dan een met het volgende email adres: <strong>{user.email}</strong>
+                                        </InfoPanel>
+                                    </Cell>
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
