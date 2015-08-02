dae382c621660428d39616171eb9d99e4c6bb78d
Robin Malfait <malfait.robin@gmail.com>
2015-08-02 19:32:00 +0200

    Adding validation on register form

--- src/js/components/users/Register.js
+++ src/js/components/users/Register.js
@@ -13,7 +13,11 @@ var Register = React.createClass({
             passwordRepeat: '',
             error: false,
             loading: false,
-            registerButton: 'Ga Verder'
+            registerButton: 'Ga Verder',
+            emailIsValid: false,
+            nameIsValid: false,
+            passwordIsValid: false,
+            passwordRepeatIsValid: false
         }
     },
     register(event) {
@@ -66,21 +70,43 @@ var Register = React.createClass({
             passwordRepeat: event.target.value
         })
     },
+    validateEmail(result) {
+        this.setState({
+            emailIsValid: result
+        })
+    },
+    validateName(result) {
+        this.setState({
+            nameIsValid: result
+        })
+    },
+    validatePassword(result) {
+        this.setState({
+            passwordIsValid: result
+        })
+    },
+    validatePasswordRepeat(result) {
+        this.setState({
+            passwordRepeatIsValid: result
+        })
+    },
     render() {
-        let { loading, registerButton } = this.state
+        let { loading, registerButton, emailIsValid, nameIsValid, passwordIsValid, passwordRepeatIsValid } = this.state
 
         let iconClass = `material-icons right ${this.state.loading ? 'icon-spin' : ''}`
+
+        let isValid = emailIsValid && nameIsValid && passwordIsValid && passwordRepeatIsValid
 
         return (
             <div>
                 <form onSubmit={this.register}>
-                    <MaterialInput label="E-mail" type="email" id="email" name="email" value={this.state.email} onChange={this.setEmail}/>
-                    <MaterialInput label="Naam" type="text" id="name" name="name" value={this.state.name} onChange={this.setName}/>
-                    <MaterialInput label="Wachtwoord" type="password" id="password" name="password" value={this.state.password} onChange={this.setPassword}/>
-                    <MaterialInput label="Herhaal Wachtwoord" type="password" id="password_repeat" name="password_repeat" value={this.state.passwordRepeat} onChange={this.setPasswordRepeat}/>
+                    <MaterialInput onValidate={this.validateEmail} validate rules={['required', 'email']} label="E-mail" type="email" id="email" name="email" value={this.state.email} onChange={this.setEmail}/>
+                    <MaterialInput onValidate={this.validateName} validate rules={['required']} label="Naam" type="text" id="name" name="name" value={this.state.name} onChange={this.setName}/>
+                    <MaterialInput onValidate={this.validatePassword} validate rules={['required']} label="Wachtwoord" type="password" id="password" name="password" value={this.state.password} onChange={this.setPassword}/>
+                    <MaterialInput onValidate={this.validatePasswordRepeat} validate rules={['required']} label="Herhaal Wachtwoord" type="password" id="password_repeat" name="password_repeat" value={this.state.passwordRepeat} onChange={this.setPasswordRepeat}/>
 
                     <div className="right-align">
-                        <button className={`btn btn-large waves-effect waves-light ${this.state.error ? 'red' : ''}`} type="submit" name="action">
+                        <button disabled={ ! isValid} className={`btn btn-large waves-effect waves-light ${this.state.error ? 'red' : ''}`} type="submit" name="action">
                             <i className={iconClass}>{loading ? 'loop' : 'send'}</i>{registerButton}
                         </button>
                     </div>
