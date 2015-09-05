ef2ffb59b9a33145bd46f982f9339d83fd0731e7
Robin Malfait <malfait.robin@gmail.com>
2015-09-05 23:56:50 +0200

    Different way to display grouplist

--- src/js/components/group/GroupList.js
+++ src/js/components/group/GroupList.js
@@ -3,6 +3,7 @@ import React, { PropTypes } from 'react'
 import Cloudinary from '../partials/Cloudinary'
 import Icon from '../partials/Icon'
 import Loading from '../loading/Loading'
+import GroupDistance from '../pages/group/GroupDistance'
 import { Card, CardContent, CardTitle } from '../card/Card'
 import { Collection, CollectionItem } from '../collection/Collection'
 import { Link } from 'react-router'
@@ -21,22 +22,29 @@ var GroupList = React.createClass({
     renderGroups(group) {
         return (
             <CollectionItem key={group.id}>
-                {group.name}
-                <Link to="group" params={{groupId: group.id, tabId: 'shouts'}} className="secondary-content">
-                    <Cloudinary
-                        image={group.logo_data}
-                        options={{ width: 24, height: 24 }}
-                        defaultElement={<span style={{
-                            position: 'absolute',
-                            left: '50%',
-                            top: '50%',
-                            transform: 'translate(-50%, -50%)',
-                            color: 'rgba(0, 0, 0, 0.4)',
-                            fontWeight: 'bold',
-                            margin: 0,
-                            fontSize: 18
-                        }}>{group.name.substr(0, 1).toUpperCase()}</span>}
-                    />
+                <Link to="group" params={{groupId: group.id, tabId: 'shouts'}}>
+                    {group.name}
+
+                    <div className="secondary-content">
+                        <Cloudinary
+                            className="right"
+                            image={group.logo_data}
+                            options={{ width: 24, height: 24 }}
+                            defaultElement={<span style={{
+                                position: 'absolute',
+                                left: '50%',
+                                top: '50%',
+                                transform: 'translate(-50%, -50%)',
+                                color: 'rgba(0, 0, 0, 0.4)',
+                                fontWeight: 'bold',
+                                margin: 0,
+                                fontSize: 18
+                            }}>{group.name.substr(0, 1).toUpperCase()}</span>}
+                        />
+                    </div>
+                    <span className="right">
+                        <GroupDistance group={group} badge/>
+                    </span>
                 </Link>
             </CollectionItem>
         )
