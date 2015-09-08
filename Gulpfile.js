d0ef365c41d8bdccdf34cefb046ad1bd195d9c0b
Robin Malfait <malfait.robin@gmail.com>
2015-09-08 11:24:50 +0200

    Cache busting files on deployment

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,22 +1,23 @@
-var gulp       = require('gulp');
-var elixir     = require('laravel-elixir');
-var livereload = require('gulp-livereload');
+var gulp        = require('gulp');
+var elixir      = require('laravel-elixir');
+var livereload  = require('gulp-livereload');
+var replace     = require('gulp-replace');
 
 elixir.config.sourcemaps = false;
 elixir.config.assetsPath = 'src';
 elixir.config.publicPath = 'dist';
 
-//elixir.config.js.browserify.transformers.push({
-//    name: 'browserify-css',
-//    options: {
-//        global: true
-//    }
-//});
-
 elixir(function (mix) {
     mix.sass('style.scss')
-        //.browserify('main.js')
-        .copy('./src/img/**/*.*', './dist/img/');
+        .copy('./src/img/**/*.*', './dist/img/')
+        .copy('_index.html', 'index.html')
+        .task('addDeploymentTimestamps');
+});
+
+gulp.task('addDeploymentTimestamps', function() {
+    gulp.src('index.html')
+        .pipe(replace(/DEPLOYED_AT/g, +new Date()))
+        .pipe(gulp.dest('.'));
 });
 
 gulp.on('task_start', function (e) {
