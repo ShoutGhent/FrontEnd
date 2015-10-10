6041d2325388f68c19e505fb00ed167d2ec302c1
Robin Malfait <malfait.robin@gmail.com>
2015-10-10 22:32:19 +0200

    Larger images

--- src/js/components/shout/ShoutImages.js
+++ src/js/components/shout/ShoutImages.js
@@ -32,25 +32,47 @@ var ShoutImages = React.createClass({
             lightboxIsOpen: false
         })
     },
+    renderThumbnail(image, i, size) {
+        let options = {
+            width: size,
+            height: size
+        }
+
+        if ( ! size) {
+            options = {}
+        }
+
+        return (
+            <a key={i} target="_blank" href={image.data.secure_url} onClick={this.openLightbox.bind(this, i)}>
+                <li style={{display: 'inline-block', margin: '0 3px'}}>
+                    <Cloudinary
+                        style={{cursor: 'pointer', maxWidth: '100%'}}
+                        image={image.data}
+                        options={options}
+                    />
+                </li>
+            </a>
+        )
+    },
     renderGallery()  {
         let { images, toBeUploaded, size } = this.props
 
-        return images && images.length > 0 ? (
-            <ul style={{display: 'inline-block', margin: 10}}>
-                {images.map((image, i) => (
-                    <a target="_blank" href={image.data.secure_url} onClick={this.openLightbox.bind(this, i)}>
-                        <li style={{display: 'inline-block', margin: '0 3px'}}>
-                            <Cloudinary
-                                style={{cursor: 'pointer'}}
-                                image={image.data}
-                                options={{width: size, height: size}}
-                            />
-                        </li>
-                    </a>
-                ))}
+        if (images) {
+            switch(images.length) {
+                case 1:
+                case 2:
+                case 4:
+                    size = false
+                    break;
+            }
+        }
 
-                {toBeUploaded.map(image => (
-                    <li style={{display: 'inline-block', margin: '0 3px', width: 65, height: 65}}>
+        return images && images.length > 0 ? (
+            <ul style={{display: 'inline-block', margin: 10, maxWidth: '100%'}} className={`image-gallery-${images.length}`}>
+                {images.map((img, i) => this.renderThumbnail(img, i, size))}
+
+                {toBeUploaded.map((image, i) => (
+                    <li key={i} style={{display: 'inline-block', margin: '0 3px', width: 65, height: 65}}>
                         <div className="relative">
                             <div className="center-both" style={{
                                 backgroundColor: 'rgba(255, 255, 255, 0.6)',
